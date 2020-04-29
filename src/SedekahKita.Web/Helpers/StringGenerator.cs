4e0ebe9fa56296d1354f613f8a959f3a4d61a04a
Mif Masterz <mifmasterz@yahoo.com>
2020-04-29 15:19:33 +0700

    fixing resizer and fname generator

---
+++ src/SedekahKita.Web/Helpers/StringGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SedekahKita.Web.Helpers
+{
+    public class StringGenerator
+    {
+        public static Random random = new Random(Environment.TickCount);
+        public static string RandomString(int length)
+        {
+            const string pool = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = pool[random.Next(0, pool.Length)];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
